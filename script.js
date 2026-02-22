249ae616f10ee5701f6edc94d411e9f82d5ee4df
sakibahamedsihab <sakiba505@gmail.com>
2026-02-22 21:54:30 +0600

    a type was killing my head, interview <- interivew. This kind of bugs are hardest to find

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ const allCards = document.querySelectorAll('#all-card-container .info-card')
 
 
 // TAB FILTER FUNCTION
-function tabFilter() {
+function tabFilter(navBtnPressed) {
 
 }
 
 // CALCULATE COUNT FUNCTION
 function calculateCount(){
-    
+
 }
 
 allCards.forEach((card) => {
@@ -25,27 +25,49 @@ allCards.forEach((card) => {
 
     interviewBtn.addEventListener('click', function() {
         card.setAttribute('data-type', 'interview');
+        card.classList.remove('border-l-6', 'border-red-400', 'rounded-l-2xl')
+        card.classList.add('border-l-6', 'border-green-400', 'rounded-l-2xl')
         state.classList.remove('border', 'border-red-800', 'text-red-800', 'bg-red-100')
         state.classList.add('border', 'border-green-800', 'text-green-800', 'bg-green-100');
         state.textContent = 'Interview';   
         
-        tabFilter();
         calculateCount();
     })
 
     rejectedBtn.addEventListener('click', function() {
         card.setAttribute('data-type', 'rejected');
+        card.classList.remove('border-l-6', 'border-green-400', 'rounded-l-2xl')
+        card.classList.add('border-l-6', 'border-red-400', 'rounded-l-2xl')
         state.classList.remove('border', 'border-green-800', 'text-green-800', 'bg-green-100');
         state.classList.add('border', 'border-red-800', 'text-red-800', 'bg-red-100')
         state.textContent = 'Rejected';
 
-        tabFilter();
         calculateCount();
     })
 
     deleteBtn.addEventListener('click', function() {
         card.remove();
-        tabFilter();
         calculateCount();
     })
 })
+
+allNavBtns.forEach((navBtn => {
+    navBtn.addEventListener('click', function() {
+        const navBtnPressed = navBtn.textContent.trim().toLowerCase();
+        console.log(navBtnPressed)
+        allNavBtns.forEach((btn) => {
+            btn.classList.remove('bg-[#422ad5]', 'text-white')
+        })
+        navBtn.classList.add('bg-[#422ad5]', 'text-white')
+
+
+        
+        // allCards.forEach((card) => {
+        //     if(navBtn.textContent === 'All' || navBtn.textContent === card.getAttribute('data-type')){
+        //         card.classList.remove('hidden');
+        //     } else {
+        //         card.classList.add('hidden')
+        //     }
+        // })
+    })
+}))
